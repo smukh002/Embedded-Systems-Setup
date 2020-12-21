9bbe5a7c6b8099c3a39f3438d02a563a641d83cb
smukh002 <46467209+smukh002@users.noreply.github.com>
2020-12-21 12:55:54 -0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,12 +1,3 @@
-/*	Author: smukh002
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab # 2 Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
